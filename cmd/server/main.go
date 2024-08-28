de0619b1f491f6a0cf44fb2357c3dd51ad145241
Gustavo B. <dsn4@wide.com.vc>
2024-08-28 13:07:07 -0300

    build: add custom port via .env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"backendProject/routes"
 
@@ -16,10 +17,11 @@ func init() {
 }
 
 func main() {
+	port := os.Getenv("SERVER_PORT")
 	r := routes.NewRouter()
 
-	log.Print("listening :8080")
-	err := http.ListenAndServe(":8080", r)
+	log.Printf("listening :%s", port)
+	err := http.ListenAndServe(":"+port, r)
 	if err != nil {
 		log.Fatalf("error starting server: %v", err)
 	}
