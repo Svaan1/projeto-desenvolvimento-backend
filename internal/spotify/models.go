691f0f827975d412bffe115439d4b9470fa2d1aa
Gustavo B. <dsn4@wide.com.vc>
2024-08-28 13:09:23 -0300

    feat: polish spotify models

--- internal/spotify/models.go
+++ internal/spotify/models.go
@@ -7,26 +7,52 @@ type SpotifyAuthResponse struct {
 }
 
 type Album struct {
-	Name    string `json:"name"`
-	Artists []struct {
-		Name string `json:"name"`
-	} `json:"artists"`
+	Id          string             `json:"id"`
+	Name        string             `json:"name"`
+	Artists     []SimplifiedArtist `json:"artists"`
+	ReleaseDate string             `json:"release_date"`
+	Images      []struct {
+		URL string `json:"url"`
+	} `json:"images"`
 }
 type AlbumResponse struct {
 	Albums []Album `json:"albums"`
 }
 
 type Track struct {
-	Name  string `json:"name"`
-	Album Album  `json:"album"`
+	Id         string `json:"id"`
+	Album      Album  `json:"album"`
+	Name       string `json:"name"`
+	PreviewURL string `json:"preview_url"`
 }
 type TrackResponse struct {
 	Tracks []Track `json:"tracks"`
 }
 
 type Artist struct {
+	Id        string   `json:"id"`
+	Name      string   `json:"name"`
+	Genres    []string `json:"genres"`
+	Followers struct {
+		Total int `json:"total"`
+	} `json:"followers"`
+}
+type SimplifiedArtist struct {
+	Id   string `json:"id"`
 	Name string `json:"name"`
 }
 type ArtistResponse struct {
 	Artists []Artist `json:"artists"`
+}
+
+type SearchResponse struct {
+	Albums struct {
+		Items []Album `json:"items"`
+	} `json:"albums"`
+	Tracks struct {
+		Items []Track `json:"items"`
+	} `json:"tracks"`
+	Artists struct {
+		Items []Artist `json:"items"`
+	} `json:"artists"`
 }
